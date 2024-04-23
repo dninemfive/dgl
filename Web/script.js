39617ce9645af4e88ebdf10cf3746bb8cd56eb76
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-04-23 11:56:26 -0700

    canvas working in typescript

--- Web/script.js
+++ Web/script.js
@@ -1,2 +1,9 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API
-var message = 'hello world';
+var canvas = document.getElementById("canvas");
+// https://stackoverflow.com/a/43525969
+// https://kernhanda.github.io/tutorial-typescript-canvas-drawing/
+if (!(canvas instanceof HTMLCanvasElement))
+    throw new Error("canvas is not an HTMLCanvasElement!");
+var context = canvas.getContext("2d");
+context.fillStyle = "black";
+context.fillRect(10, 10, 10, 10);
--- Web/script.ts
+++ Web/script.ts
@@ -1,2 +1,9 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API
-let message: string = 'hello world'
+const canvas = document.getElementById("canvas");
+// https://stackoverflow.com/a/43525969
+// https://kernhanda.github.io/tutorial-typescript-canvas-drawing/
+if(!(canvas instanceof HTMLCanvasElement))
+    throw new Error("canvas is not an HTMLCanvasElement!")
+let context: CanvasRenderingContext2D = canvas.getContext("2d")
+context.fillStyle = "black"
+context.fillRect(10, 10, 10, 10)
