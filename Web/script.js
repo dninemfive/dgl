fae81e12965303e0af8f21332904e4cee76eb085
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-04-23 11:30:55 -0700

    start JS implementation since MAUI is frustrating

---
+++ Web/script.js
@@ -0,0 +1,5 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+ctx.fillStyle = "black";
+ctx.fillRect(10, 10, 1, 1);
